2bd8b9a053f43384b36383e02f118b1202941406
MiyueSC <913784771@qq.com>
2024-02-26 16:11:01 +0800

    [other](theme-config): add the zh repo

--- book/online-book/.vitepress/theme/index.ts
+++ book/online-book/.vitepress/theme/index.ts
@@ -7,6 +7,7 @@ export default {
   setup() {
     const { lang } = useData()
     if (inBrowser) {
+      // @ts-ignore
       document.cookie = `nf_lang=${lang.value}; expires=Mon, 1 Jan 2024 00:00:00 UTC; path=/`
     }
   },
